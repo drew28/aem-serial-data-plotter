58d9ebb7407a6c638512464a8ed5ee7306576c19
atreid@gmail.com <atreid@gmail.com@59acf406-e25a-c7c4-faa9-04e995ee8adf>
2012-09-18 00:26:28 +0000

    New menu items start and stop logging are added.

---
+++ trunk/DataGrapher/src/Datalogger.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+
+/**
+ *
+ * @author Drew
+ */
+public class Datalogger {
+    
+    public Datalogger() {
+        System.out.println("Creating reader for datalogger.");
+        
+        System.out.println("Creating writer for datalogger.");
+        
+        System.out.println("Creating fie for datalogger.");
+        
+        System.out.println("Waiting for data.");
+        
+    }
+    
+    public void stop() {
+        System.out.println("Stop datalogging.");
+    }
+}
